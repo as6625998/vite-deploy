d9460a4fc1ca7c1c99d44dc1e3e8adf33153426d
as6625998 <as6625998@gmail.com>
2023-02-18 11:56:53 +0800

    git init

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        這是一張卡片<br>
+        {{text}}
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                text:'這是一個文字'
+            }
+        }
+    }
+</script>
